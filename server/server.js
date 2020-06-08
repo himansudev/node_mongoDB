4752297903649f6b880c648c6d595bc1bae1fc73
himansudev <himansu.developer@gmail.com>
2020-06-08 22:19:00 +0530

    add get route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,15 @@ app.post('/todos', (req, res) => {
   })
 });
 
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (error) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started On Port 3000');
 });
